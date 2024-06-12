bfcb70add450258ae4afbf8f63745bd84900794b
Valeri Nyshenko <vani280301@gmail.com>
2024-06-12 04:45:45 +0300

    Task2 - edited. Bunny - added new. TextHighlight

--- main.js
+++ main.js
@@ -27,20 +27,31 @@ function checkNumber() {
 
 // TASK 2
 
-function checkAnswer(buttonId) {
+// const optionButtonsForm = document.getElementById(optionButtonsForm);
+
+// optionButtonsForm.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   checkAnswer();
+// });
+
+function checkAnswer(event, buttonId) {
+  event.preventDefault();
+
   const btnWrong1 = document.getElementById("btnWrong1");
   const btnWrong2 = document.getElementById("btnWrong2");
   const btnWrong3 = document.getElementById("btnWrong3");
   const btnCorrect = document.getElementById("btnCorrect");
+  const hiddenPart = document.getElementById("hidden");
 
-  if (
-    buttonId === "btnWrong1" ||
-    buttonId === "btnWrong2" ||
-    buttonId === "buttonWrong3"
-  ) {
-    // document.getElementById(buttonId).style.
-    errorMessage.style.display = "flex";
-  } else {
+  if (buttonId === btnCorrect) {
     errorMessage.style.display = "none";
+    hiddenPart.style.display = "flex";
+  } else if (
+    buttonId === btnWrong1 ||
+    buttonId === btnWrong2 ||
+    buttonId === btnWrong3
+  ) {
+    hiddenPart.style.display = "none";
+    errorMessage.style.display = "flex";
   }
 }
